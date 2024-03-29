84cd660886d150351eb22b226481cabf76235f92
rougho <rohi@rgho.de>
2024-03-29 11:46:17 +0100

    get json & update, & extract msg and id, and echo msg

--- echobot.py
+++ echobot.py
@@ -1,6 +1,5 @@
 import os
 import json
-import logging
 from dotenv import load_dotenv
 import requests
 
@@ -11,26 +10,44 @@ TOKEN = os.getenv('HTTP_API_TOKEN')
 URL = f"https://api.telegram.org/bot{TOKEN}/"
 
 
-def get_url(url):
-    """ Fetches the content of the provided URL and returns it as a decoded string.
-
-    This function uses the `requests` library to make an HTTP GET request to the specified URL.
-    If successful, it decodes the response content using UTF-8 encoding and returns the decoded string.
-
-    Args:
-        url (str): The URL to fetch content from.
-
-    Returns:
-        str: The decoded content of the URL, or None if an error occurs.
-
-    """
+def get_url(url: str) -> str:
     try:
         respone = requests.get(url)
+        # respone.raise_for_status()
         content = respone.content.decode('utf8')
         return content
     except requests.exceptions.RequestException as e:
-        print("error: ", e)
+        print(f"error: {e}")
+        return None
 
 
-def get_json(url):
-    pass
+def get_json(url: str) -> dict:
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_update() -> dict:
+    url = URL + "getUpdates"
+    js = get_json(url)
+    return js
+
+
+def get_last_chat(updates: dict) -> tuple:
+    number_of_updates = len(updates['result'])
+    last_update = number_of_updates - 1
+    text = updates.get('result', [])[last_update].get(
+        'message', {}).get('text', 0)
+    chat_id = updates.get('result', [])[last_update].get(
+        'message', {}).get('chat').get('id', 0)
+    return (text, chat_id)
+
+
+def send_message(text: str, chat_id: str) -> None:
+    url = URL + f"sendMessage?text={text}&chat_id={chat_id}"
+    get_url(url)
+
+
+text, chat = get_last_chat(get_update())
+print(chat, text)
+send_message(text, chat)

69bbd2c8e546da8a52d67d01cbcb3f620baf57a0
rougho <rohi@rgho.de>
2024-04-06 16:16:22 +0200

    fine-tuned ollama llama2

--- model/tuner.py
+++ model/tuner.py
@@ -1,13 +1,31 @@
+from ollama import AsyncClient
+import asyncio
 import os
+import sys
 from merger import PDF_DIR, MERGED_PDF_FILENAME, merger_runner
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv
+from langchain_community.llms import Ollama
+# import ollama
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
+from langchain_community.embeddings import GPT4AllEmbeddings
+# from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+# from langchain_community.vectorstores import chroma
+# from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain_community.chat_models import ollama
+# from langchain_community.chat_models import ChatOllama
+# from langchain_core.runnables import RunnablePassthrough
+# from langchain_core.output_parsers import StrOutputParser
+# import huggingface_hub
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+# from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.llms import HuggingFaceHub
+# # from langchain_community import embeddings
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_community.embeddings import OllamaEmbeddings
+from langchain.chains import RetrievalQA
+
 
 ENV_DIR = os.path.join(os.getcwd(), '.env')
 PDF_DOC = os.path.join(PDF_DIR, MERGED_PDF_FILENAME)
@@ -26,55 +44,39 @@ def get_pdf_text(pdf) -> str:
     # return text
 
 
-def get_text_chunks(text):
-    spliter = RecursiveCharacterTextSplitter(
-        # separators="\n",
-        separators=" ",
-        chunk_size=1000,
-        chunk_overlap=100,
-        length_function=len,
-    )
-    chunks = spliter.split_text(text)
-    return chunks
-
-
-def get_vectorestore(text_chunks):
-    embeddings = HuggingFaceInstructEmbeddings(
-        model_name="hkunlp/instructor-xl")
-
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-
-    return vectorstore
-
-
-def get_coversation_chain(vectorstore):
-    llm = ollama()
-    memory = ConversationBufferMemory(
-        memory_key='chat history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
-
-def main():
-    raw_text = get_pdf_text(PDF_DOC)
-    text_chunks = get_text_chunks(raw_text)
-    vectorstore = get_vectorestore(text_chunks)
-    conversation = get_coversation_chain(vectorstore)
-
-    print("Welcome! Chat with the embedded model. Type 'quit' to exit.")
-
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == "quit":
-            break
-
-        response = conversation(user_input)
-        print("Model:", response)
-
-
 if __name__ == "__main__":
-    main()
+    ollama = Ollama(base_url='http://localhost:11434', model='llama2')
+
+    loader = PyPDFLoader(PDF_DOC)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(separators=["\n", "\n\n", " "],
+                                                                         chunk_size=500, chunk_overlap=20)
+    text = text_splitter.split_documents(documents)
+    vectorstore = Chroma.from_documents(
+        documents=text, embedding=GPT4AllEmbeddings())
+
+    # qachain = RetrievalQA.from_chain_type(
+    #     ollama, retriver=vectorstore.as_retriever())
+
+    # q = "what is Inheritance Tax?"
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        ollama,
+        retriever=vectorstore.as_retriever(),
+        return_source_documents=False
+    )
+
+    chat_history = []
+    while True:
+        query = input('Prompt: ')
+        if query.lower() in ["exit", "quit", "q"]:
+            print('Exiting')
+            sys.exit()
+        result = qa_chain({'question': query, 'chat_history': chat_history})
+        print('Answer: ' + result['answer'] + '\n')
+        chat_history.append((query, result['answer']))
+    # async def chat():
+    #     message = {'role': 'user', 'content': 'Why is the sky blue?'}
+    #     async for part in await AsyncClient().chat(model='llama2', messages=[message], stream=True):
+    #         print(part['message']['content'], end='', flush=True)
+
+    # asyncio.run(chat())
